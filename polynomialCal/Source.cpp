7955c37bf5078a405add32ba9f226bf08bea637b
NhanDang <dnhan968@gmail.com>
2023-05-15 01:57:09 +0700

    third_commit

--- polynomialCal/Source.cpp
+++ polynomialCal/Source.cpp
@@ -22,7 +22,7 @@ public:
 class Tree {
 public:
 	node *head;
-
+public:
 	//init tree
 	Tree() {
 		head = NULL;
@@ -46,6 +46,37 @@ public:
 	node *CreateTree(string s, node *x, node *y, node *z) {
 		for (int i = 0; i < s.size(); i++) {
 			if (s[i] == '+' || s[i] == '*' || s[i] == '/' || s[i] == '^') {
+				z = new node(s[i]);
+				x = POP();
+				y = POP();
+				z->left = y;
+				z->right = x;
+				PUSH(z);
+			}
+			else {
+				z = new node(s[i]);
+				PUSH(z);
+			}
+		}
+	}
+};
+
+class Cal {
+public:
+	string dataIn;
+public:
+
+	void inputPol() {
+		cout << "Moi nhap vao da thuc: ";
+		cin.ignore();
+		getline(cin, dataIn);
+	}
+
+	string dataPreProcess(string s, int i) {
+		int j = i + 1, count;
+		if (s[i] == '-' && s[i + 1] == '(') {
+			char after = s[i + 2]; // luu ky tu sau ngoac
+			for (j; s[j] != ')'; j++) {
 
 			}
 		}
